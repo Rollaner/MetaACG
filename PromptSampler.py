7750f57da2fbaef18ff95f0b8b1a2518ef50c682
Rollaner <sebastianattoni@gmail.com>
2025-08-06 13:01:57 -0400

    Implementacion prompt sampler y refactorizacion bases de datos

--- PromptSampler.py
+++ PromptSampler.py
@@ -1,8 +1,108 @@
-def sampleComponentDB():
-    return 0
+import random
+import itertools
+from string import Template
 
-def samplePromptDB():
-    return 0
+templateSeed= Template("""TASK:GENERATE_COMPONENT_ONE.REP,NB,EVAL
+PROBLEM_DEF:
+---
+$problema
+---
+OUTPUT_FORMAT_STRICT:
+"R_STR","NB_CODE","E_CODE"
+R_STR_DESC:STRING.SOL_ENCODE.EX:BIN_STR,PERM_LIST,TREE_STR.
+NB_CODE_DEF:PYTHON_FUNC.NAME=generate_neighbour.ARGS=1(solution).OP_NEIGHBOR_SOLUTION.SIG=def generate_neighbour(solution):
+E_CODE_DEF:PYTHON_FUNC.NAME=evaluate_solution.ARGS=1(solution).RET_NUM_FITNESS.SIG=def evaluate_solution(solution):
+CRITICAL_INSTRUCTIONS:
+1.CODE_SYNTAX_CORRECT_FUNC_SELF_CONTAINED.
+2.NO_ADD_TEXT_EXPLANATION_FORMAT_OUTSIDE_CSV.
+3.SOL_ARG_TYPE_MATCH_R_STR.
+INSPIRATIONS:
+$inspirations
+""")
 
-def sampleProblemDB():
-    return 0
+templateUpdate= Template("""TASK:GENERATE_COMPONENT_ONE.REP,NB,EVAL
+PROBLEM_DEF:
+---
+$problema
+---
+OUTPUT_FORMAT_STRICT:
+"R_STR","NB_CODE","E_CODE"
+R_STR_DESC:STRING.SOL_ENCODE.EX:BIN_STR,PERM_LIST,TREE_STR.
+NB_CODE_DEF:PYTHON_FUNC.NAME=generate_neighbour.ARGS=1(solution).OP_NEIGHBOR_SOLUTION.SIG=def generate_neighbour(solution):
+E_CODE_DEF:PYTHON_FUNC.NAME=evaluate_solution.ARGS=1(solution).RET_NUM_FITNESS.SIG=def evaluate_solution(solution):
+CRITICAL_INSTRUCTIONS:
+1.CODE_SYNTAX_CORRECT_FUNC_SELF_CONTAINED.
+2.NO_ADD_TEXT_EXPLANATION_FORMAT_OUTSIDE_CSV.
+3.SOL_ARG_TYPE_MATCH_R_STR.
+INSPIRATIONS:
+$inspirations                         
+METAHEURSITIC USED AND RESULTS
+$results
+FEEDBACK:
+$feedback
+""")
+
+def sampleComponenteDB(componenteDB, problemaID,seed):
+
+    problemSubset = componenteDB[componenteDB['ProblemaID'] == problemaID]
+    representaciones = componenteDB.columns[1]
+    representacionesPosibles = random.choice(list(problemSubset[representaciones].unique()))
+    matches = problemSubset[problemSubset[representaciones] == representacionesPosibles]
+    if len(matches) == 0:
+        return matches  # or return None / empty DF / some sentinel
+    tamaño = min(5, len(matches))
+    return matches.sample(n=tamaño, random_state=seed)
+
+def sampleFeedbackDB(feedbackDB, problemaID, componentSample): ## La logica es la misma de momento, se separan las funciones en caso de que se necesite modificar a futuro
+    feedbackSubset = feedbackDB[feedbackDB['ProblemaID'] == problemaID]
+    versionesComponente = componentSample.iloc[:, 4]
+    feedback = feedbackSubset[feedbackSubset.iloc[:,1].isin(versionesComponente)]
+    return feedback
+
+def sampleResultadoDB(resultadoDB, problemaID, componentSample): ## To-Do revisar que la sintaxis este correcta
+    resultadoSubset = resultadoDB[resultadoDB['ProblemaID'] == problemaID]
+    versionesComponente = componentSample.iloc[:, 4]
+    resultados = resultadoSubset[resultadoSubset.iloc[:,1].isin(versionesComponente)]
+    return resultados
+
+def sampleProblemaDB(problemaDB,seed):
+    return problemaDB.sample(n=1, random_state=seed) 
+
+def combinarComponentes(db):
+    ComponenteVecindad = db.iloc[:,2].astype(str).unique().tolist()
+    ComponenteEvaluacion = db.iloc[:,3].astype(str).unique().tolist()
+    combinaciones = list(itertools.product(ComponenteVecindad,ComponenteEvaluacion))
+    return combinaciones
+
+def generarStrings(dataframe):
+    if 'Text' in dataframe.columns:
+        return  "\n".join(dataframe['Text'].astype(str).tolist())
+    else: return f"{len(dataframe)} items found."
+
+def generateSeedPrompt(problemaSample,componenteDB,seed):
+    random.seed(seed)
+    problemaID = problemaSample.iloc[0,0]
+    componentSample = sampleComponenteDB(componenteDB, problemaID,seed)
+    assert componentSample.iloc[:, 0].nunique() <= 1, "Multiples problemas diferentes seleccionados"
+    assert componentSample.iloc[:, 1].nunique() <= 1, "Multiples representaciones differentes seleccionadas"
+    combinaciones = combinarComponentes(componentSample) #revisar si esto es imprimible como string
+    inspiraciones = ", ".join(f"{v} - {e}" for v, e in combinaciones)
+    prompt = templateSeed.safe_substitute(problema=problemaSample.iloc[0,1], inspirations=inspiraciones) 
+    return prompt
+
+
+
+def updatePrompt(problemaSample, componenteDB, resultDB, feedbackDB, seed): ## podriamos optimizar po medio de almacenar un cache de las variables que se vuelven a calcular
+    random.seed(seed)
+    problemaID = problemaSample.iloc[0,0]
+    componentSample = sampleComponenteDB(componenteDB, problemaID,seed)
+    assert componentSample.iloc[:, 0].nunique() <= 1, "Multiples problemas diferentes seleccionados"
+    assert componentSample.iloc[:, 1].nunique() <= 1, "Multiples representaciones differentes seleccionadas"
+    resultSample = sampleResultadoDB(resultDB, problemaID, componentSample)
+    feedbackSample = sampleFeedbackDB(feedbackDB, problemaID, componentSample)
+    resultados = generarStrings(resultSample)
+    feedback = generarStrings(feedbackSample) 
+    combinaciones = combinarComponentes(componentSample)
+    inspiraciones = ", ".join(f"{v} - {e}" for v, e in combinaciones)
+    prompt = templateUpdate.safe_substitute(problema=problemaSample.iloc[0,1], inspirations=inspiraciones, resultados=resultados, feedback=feedback) 
+    return prompt
